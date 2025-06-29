5798fbba35669e81cf87f09615c1456ab2ac4deb
wuyijun <wuyijun1129@hotmail.com>
2025-06-29 18:27:38 +0800

    set scan timeout to 3 seconds instead of 5

--- app/src/main/java/com/android/blerc/common/BleDeviceManager.java
+++ app/src/main/java/com/android/blerc/common/BleDeviceManager.java
@@ -27,7 +27,7 @@ public class BleDeviceManager {
         if (context == null) {
             return;
         }
-        ViseBle.config().setScanTimeout(5000).setScanRepeatInterval(BleConstant.DEFAULT_CONN_TIME).setConnectTimeout(PathInterpolatorCompat.MAX_NUM_POINTS).setOperateTimeout(BleConstant.DEFAULT_OPERATE_TIME).setConnectRetryCount(1).setConnectRetryInterval(1000).setOperateRetryCount(2).setOperateRetryInterval(1000).setMaxConnectCount(3);
+        ViseBle.config().setScanTimeout(3000).setScanRepeatInterval(BleConstant.DEFAULT_CONN_TIME).setConnectTimeout(PathInterpolatorCompat.MAX_NUM_POINTS).setOperateTimeout(BleConstant.DEFAULT_OPERATE_TIME).setConnectRetryCount(1).setConnectRetryInterval(1000).setOperateRetryCount(2).setOperateRetryInterval(1000).setMaxConnectCount(3);
         ViseBle.getInstance().init(context.getApplicationContext());
     }
 
